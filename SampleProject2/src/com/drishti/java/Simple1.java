13368e88c8dea43fb14c81f4285bd0d05b589000
sadadure123 <84184305+sadadure123@users.noreply.github.com>
2021-05-15 18:15:10 +0530

    update data

--- SampleProject2/src/com/drishti/java/Simple1.java
+++ SampleProject2/src/com/drishti/java/Simple1.java
@@ -4,5 +4,6 @@ public class Simple1 {
 	public static void main (String args[]) {
 		System.out.println("Welcome to");
 		System.out.println("Welcome to eclipse");
+		System.out.println("Welcome to eclipse 1");
 }
 }
